e5d427743d3ed5bc2fa2216d881eaedb743e0e10
Albdarion <ukiteroamano968@gmail.com>
2019-10-15 12:35:12 +0200

    Model ciekawostki

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Ciekawostki
 
 # Register your models here.
 
+
+@admin.register(Ciekawostki)
+class CiekawostkiAdmin(admin.ModelAdmin):
+    list_display = ('liczbaporzadkowa', 'ciekawostka')
+    fields = ['liczbaporzadkowa', 'ciekawostka']
--- main_page/models.py
+++ main_page/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
+class Ciekawostki(models.Model):
+    liczbaporzadkowa = models.CharField(max_length=200)
+    ciekawostka = models.TextField(name="ciekawostka")
+
+
